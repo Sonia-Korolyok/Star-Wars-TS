b721daa14ecd7031d261049adcc72236c2e94624
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-07-30 19:32:17 +0300

    Hero pic to home added

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -31,7 +31,7 @@ export const characters: Characters = {
     },
     leia:{
         name: "Leia Organa",
-        img: friend6,
+        img: friend9,
         url: `${base_url+version}/peoples/5`
     },
     obi_wan:{
@@ -51,7 +51,7 @@ export const characters: Characters = {
     },
     yoda:{
         name: "Yoda",
-        img: friend9,
+        img: friend6,
         url: `${base_url+version}/peoples/0`
     },
     ewok:{
