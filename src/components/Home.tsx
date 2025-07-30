5f8b47a0a9f52249110cf3b8904666d89d5e807e
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-07-30 20:22:27 +0300

    routing and navigation in about me and home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,22 @@
 import Hero from "./Hero.jsx";
 import DreamTeam from "./DreamTeam.jsx";
 import OpeningCrawl from "./OpeningCrawl.jsx";
+import {characters, defaultHero} from "../utils/constants.ts";
+import {useParams} from "react-router";
+import {useContext, useEffect} from "react";
+import {SWContext} from "../utils/context.ts";
 
 const Home = () => {
+    let {heroId = defaultHero} = useParams();
+    const {changeHero} = useContext(SWContext);
+
+    useEffect(() => {
+        if(!(heroId in characters)){
+            heroId = defaultHero;
+        }
+        changeHero(heroId);
+    }, [heroId]);
+
     return (
         <main>
             <Hero/>
--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,16 +1,18 @@
 import Button from "./ui/Button.jsx";
 import {NavLink} from "react-router";
+import {useContext} from "react";
+import {SWContext} from "../utils/context.ts";
 
 interface NavItemsProps {
     itemTitle: string
 }
 
 const NavItem = ({itemTitle}: NavItemsProps) => {
-
+const {hero} = useContext(SWContext);
 
     return (
 
-            <NavLink to={`${itemTitle}`}><Button>{itemTitle}</Button></NavLink>
+            <NavLink to={`${itemTitle}/${hero}`}><Button>{itemTitle}</Button></NavLink>
 
 
     )
